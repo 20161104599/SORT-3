558c85f53e0ec509fcd45b90ca8e5dee583fcec0
20161104599 <1416176774@qq.com>
2017-06-19 09:43:10 +0800

    gps
    gps

--- SORT     3/main.c
+++ SORT     3/main.c
@@ -6,10 +6,32 @@
 //  Copyright © 2017年 20161104599. All rights reserved.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+
+
+#include <stdio.h>
+int main()
+{
+    
+    char time[10],gps,latitude1,longitude1,yeae[10],s[5];
+    double latitude,longitude,v,course;
+    
+    FILE *fw;
+    fw=fopen("//Users//z20161104599//Desktop//SORT     3//output.txt","r");
+    if(fw==NULL)
+    {
+        printf("打开文件错误，要打开的文件可能不存在.")
+    }
+    else
+    {
+        fscanf(")
+    }
+                 fprintf(fw,"\n");
+        printf("\n");
+        
+
+    fclose(fw);
+    
     return 0;
 }
