d025ed2e4dbb0ff72f391664f7f74e421c972d7b
20161104599 <1416176774@qq.com>
2017-06-27 11:52:52 +0800

    gps
    gps

--- SORT     3/main.c
+++ SORT     3/main.c
@@ -14,6 +14,7 @@
 int main()
 {
     FILE *fp1;
+    FILE *fp2;
     char str1[65];
     char str2[65];
     char lat[20];
@@ -24,44 +25,58 @@ int main()
     char head[20];
     int i;
     fp1=fopen("//Users//a20161104597//Desktop//SORT 3//GPS170510.log","r+");
-    fscanf(fp1,"%s%s",str1,str2);
-    printf("结果: %s\n%s\n",str1,str2);
-    for(i=0;i<8;i++)
-        lat[i]=str1[i+16];
-    lat[8]='\0';
-    printf("纬度：%s\n",lat);
-    for(i=0;i<6;i++)
-        time[i]=str1[i+7];
-    time[6]='\0';
-    printf("时间：%s\n",time);
-    for(i=0;i<9;i++)
-        lon[i]=str1[i+27];
-    lon[9]='\0';
-    printf("经度：%s\n",lon);
-    for(i=0;i<5;i++)
-        v[i]=str1[i+39];
-    v[5]='\0';
-    printf("地面速率：%s\n",v);
-    for(i=0;i<5;i++)
-        head[i]=str1[i+45];
-    head[5]='\0';
-    printf("地面航向：%s\n",head);
-    for(i=0;i<6;i++)
-        date[i]=str1[i+51];
-    date[6]='\0';
-    printf("日期：%s\n",date);
-    for(i=0;i<8;i++)
-        lat[i]=str2[i+14];
-    lat[8]='\0';
-    printf("纬度：%s\n",lat);
-    for(i=0;i<6;i++)
-        time[i]=str2[i+7];
-    time[6]='\0';
-    printf("时间：%s\n",time);
-    for(i=0;i<9;i++)
-        lon[i]=str2[i+25];
-    lon[9]='\0';
-    printf("经度：%s\n",lon);
-    fclose(fp1);
+    fp2=fopen("//Users//a20161104597//Desktop//SORT 3//GPS170510.xls","w");
+    while(fscanf(fp1,"%s%s",str1,str2)!=EOF)
+    {
+        printf("结果: %s\n%s\n",str1,str2);
+        fprintf(fp2,"结果: %s\n%s\n",str1,str2);
+        for(i=0;i<8;i++)
+            lat[i]=str1[i+16];
+        lat[8]='\0';
+        printf("纬度：%s\n",lat);
+        fprintf(fp2,"纬度：%s\n",lat);
+        for(i=0;i<6;i++)
+            time[i]=str1[i+7];
+        time[6]='\0';
+        printf("时间：%s\n",time);
+        fprintf(fp2,"时间：%s\n",time);
+        for(i=0;i<9;i++)
+            lon[i]=str1[i+27];
+        lon[9]='\0';
+        printf("经度：%s\n",lon);
+        fprintf(fp2,"经度：%s\n",lon);
+        for(i=0;i<5;i++)
+            v[i]=str1[i+39];
+        v[5]='\0';
+        printf("地面速率：%s\n",v);
+        fprintf(fp2,"地面速率：%s\n",v);
+        for(i=0;i<5;i++)
+            head[i]=str1[i+45];
+        head[5]='\0';
+        printf("地面航向：%s\n",head);
+        fprintf(fp2,"地面航向：%s\n",head);
+        for(i=0;i<6;i++)
+            date[i]=str1[i+51];
+        date[6]='\0';
+        printf("日期：%s\n",date);
+        fprintf(fp2,"日期：%s\n",date);
+        for(i=0;i<8;i++)
+            lat[i]=str2[i+14];
+        lat[8]='\0';
+        printf("纬度：%s\n",lat);
+        fprintf(fp2,"纬度：%s\n",lat);
+        for(i=0;i<6;i++)
+            time[i]=str2[i+7];
+        time[6]='\0';
+        printf("时间：%s\n",time);
+        fprintf(fp2,"时间：%s\n",time);
+        for(i=0;i<9;i++)
+            lon[i]=str2[i+25];
+        lon[9]='\0';
+        printf("经度：%s\n",lon);
+        fprintf(fp2,"经度：%s\n",lon);
+       
+    } fclose(fp1);
+        fclose(fp2);
     return 0;
 }
